89528c4af15ebb7c52128828eb92fda14ce76db9
ahnguy30 <ahnguy30@asu.edu>
2024-11-14 14:46:56 -0700

    task 1 add more test case

--- src/test/java/ser515/unittest/SimpleSquareRootClassTest.java
+++ src/test/java/ser515/unittest/SimpleSquareRootClassTest.java
@@ -15,24 +15,35 @@ class SimpleSquareRootClassTest {
     // This executes once before each test. BeforeAll executes once before ALL tests
     @BeforeEach
     void beforeEach() {
-
         myX = new SimpleSquareRootClass(1);
     }
     // There are also @AfterEach and @AfterAll with expected semantics. Usually these would do cleanup, such as
     // resetting object state after a test (each) or cleaning up hardresources like database connection (All)
-    
+
     @Test
     void testMakeSqrt() {
         assertEquals(0.0, myX.makeSqrt());
     }
 
+
     @Test
-    void testSquareRoot() {
-        assertEquals(0.0, myX.makeSqrt());
+    void testSquareRootOfPositiveNumber() {
+        assertEquals(3, SimpleSquareRootFunction.makeSqrt(9), 0.001, "Square root of 9 should be 3");
+        assertEquals(5, SimpleSquareRootFunction.makeSqrt(25), 0.001, "Square root of 25 should be 5");
     }
 
     @Test
-    void testMakeSqrtZeroAll() {
-        assertEquals(0, myX.makeSqrt());
+    void testSquareRootOfZero() {
+        assertEquals(0, SimpleSquareRootFunction.makeSqrt(0), 0.001, "Square root of 0 should be 0");
+    }
+
+    @Test
+    void testSquareRootOfNonPerfectSquare() {
+        assertEquals(2.236, SimpleSquareRootFunction.makeSqrt(5), 0.001, "Square root of 5 should be close to 2.236");
+    }
+
+    @Test
+    void testSquareRootOfNegativeNumber() {
+        assertTrue(Double.isNaN(SimpleSquareRootFunction.makeSqrt(-4)), "Square root of a negative number should return NaN");
     }
 }
